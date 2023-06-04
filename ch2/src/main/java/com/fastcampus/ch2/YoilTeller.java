42ca81187f76b967a74c71f3adc32becd55b0c42
BuildEnough <rhtmaehcl878@naver.com>
2023-06-04 13:28:42 +0900

    원격 프로그램으로 응답하기

--- ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -5,20 +5,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
 
 // 년월일을 입력하면 요일을 알려주는 원격 프로그램
-@RestController
+@Controller
 public class YoilTeller {
     @RequestMapping("/getYoil")
-    public String main(HttpServletRequest request) {
+    public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 1. 입력
         String year = request.getParameter("year");
         String month = request.getParameter("month");
         String day = request.getParameter("day");
 
-        System.out.println("year = " + year);
-        System.out.println("month = " + month);
-        System.out.println("day = " + day);
+        int yyyy = Integer.parseInt(year);
+        int mm = Integer.parseInt(month);
+        int dd = Integer.parseInt(day);
 
-        return "Monday";
+        // 2. 작업 - 요일 계산
+        Calendar cal = Calendar.getInstance(); // 현재 날짜와 시간을 갖는 cal
+        cal.clear(); // cal의 모든 필드를 초기화
+        cal.set(yyyy, mm - 1, dd); // 월(mm)은 0부터 11이기 때문에 1을 빼줘야 함
+
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); // 1~7 반환, 1:일요일, 2: 월요일 ...
+        char yoil = "일월화수목금토".charAt(dayOfWeek-1); // 1~7 -> 0~6
+
+        // 3. 출력 - 작업 결과를 브라우저에 전송
+        response.setCharacterEncoding("ms949"); // 한글 윈도우 ms 949
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("year = " + year);
+        out.println("month = " + month);
+        out.println("day = " + day);
+        out.println("yoil = " + yoil);
+        out.println("</body>");
+        out.println("</html>");
     }
 }
