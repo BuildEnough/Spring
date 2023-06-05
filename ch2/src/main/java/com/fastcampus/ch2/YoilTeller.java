545c5729ef103c588754a966100ebbb1717b4290
BuildEnough <rhtmaehcl878@naver.com>
2023-06-05 19:20:19 +0900

    MVC로 관심사 분리

--- ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -1,8 +1,10 @@
 package com.fastcampus.ch2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,38 +15,38 @@ import java.util.Calendar;
 // 년월일을 입력하면 요일을 알려주는 원격 프로그램
 @Controller
 public class YoilTeller {
-    @RequestMapping("/getYoil")
-    public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // 1. 입력
-        String year = request.getParameter("year");
-        String month = request.getParameter("month");
-        String day = request.getParameter("day");
+    @RequestMapping("/yoil")
+//    public void main(String year, String month, String day, HttpServletResponse response) throws IOException {
+//    public String main(int year, int month, int day, Model model) throws IOException {
+    public ModelAndView main(int year, int month, int day) throws IOException {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("yoilError");
 
-        int yyyy = Integer.parseInt(year);
-        int mm = Integer.parseInt(month);
-        int dd = Integer.parseInt(day);
+        if (!isValid(year, month, day)) {
+            return mv; // 사용자가 요청한 날짜가 유효하지 않으면 yoilError를 보여준다
+        }
 
         // 2. 작업 - 요일 계산
         Calendar cal = Calendar.getInstance(); // 현재 날짜와 시간을 갖는 cal
         cal.clear(); // cal의 모든 필드를 초기화
-        cal.set(yyyy, mm - 1, dd); // 월(mm)은 0부터 11이기 때문에 1을 빼줘야 함
+        cal.set(year, month - 1, day); // 월(mm)은 0부터 11이기 때문에 1을 빼줘야 함
 
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); // 1~7 반환, 1:일요일, 2: 월요일 ...
         char yoil = "일월화수목금토".charAt(dayOfWeek-1); // 1~7 -> 0~6
 
-        // 3. 출력 - 작업 결과를 브라우저에 전송
-        response.setCharacterEncoding("ms949"); // 한글 윈도우 ms 949
+        // 작업한 결과를 Model에 저장(DS가 Model을 View로 전달)
+        mv.addObject("year", year);
+        mv.addObject("month", month);
+        mv.addObject("day", day);
+        mv.addObject("yoil", yoil);
+        mv.setViewName("yoil"); // yoil.html
 
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("year = " + year);
-        out.println("month = " + month);
-        out.println("day = " + day);
-        out.println("yoil = " + yoil);
-        out.println("</body>");
-        out.println("</html>");
+        return mv;
+//        return "yoil"; // yoil.html - 뷰의 이름을 반환
+
+    }
+
+    private boolean isValid(int year, int month, int day) {
+        return true;
     }
 }
