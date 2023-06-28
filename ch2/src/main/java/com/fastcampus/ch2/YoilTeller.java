b1cdbbe952d116902c08ad446de5ae12074e0f05
BuildEnough <rhtmaehcl878@naver.com>
2023-06-28 17:47:30 +0900

    MVC로 관심사 분리하기

--- ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -1,46 +1,41 @@
 package com.fastcampus.ch2;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Calendar;
 
-@RestController
+@Controller
 public class YoilTeller {
-    @RequestMapping("/getYoil")
-    public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String year = request.getParameter("year");
-        String month = request.getParameter("month");
-        String day = request.getParameter("day");
+    @RequestMapping("/yoil")
+    public ModelAndView main(int year, int month, int day) throws IOException {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("yoilError");
 
-        int yyyy = Integer.parseInt(year);
-        int mm = Integer.parseInt(month);
-        int dd = Integer.parseInt(day);
+        if (!isVaild(year, month, day)) {
+            return mv;
+        }
 
         Calendar cal = Calendar.getInstance();
         cal.clear();
-        cal.set(yyyy, mm-1, dd);
+        cal.set(year, month-1, day);
 
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
         char yoil = "일월화수목금토".charAt(dayOfWeek-1);
 
-        response.setCharacterEncoding("ms949");
+        mv.addObject("year", year);
+        mv.addObject("month", month);
+        mv.addObject("day", day);
+        mv.addObject("yoil", yoil);
+        mv.setViewName("yoil");
 
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("</head>");
-        out.println("</body>");
-        out.println("year = " + year);
-        out.println("month = " + month);
-        out.println("day = " + day);
-        out.println("yoil = " + yoil);
-        out.println("<body>");
-        out.println("</html>");
+        return mv;
+    }
 
+    private boolean isVaild(int year, int month, int day) {
+        return true;
     }
 }
