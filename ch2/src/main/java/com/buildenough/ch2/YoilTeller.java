36de031ba8436c2317fdd74d86a895b63cd61e58
BuildEnough <rhtmaehcl878@naver.com>
2023-08-03 22:42:09 +0900

    Spring 연습

--- ch2/src/main/java/com/buildenough/ch2/YoilTeller.java
+++ ch2/src/main/java/com/buildenough/ch2/YoilTeller.java
@@ -2,45 +2,29 @@ package com.buildenough.ch2;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Calendar;
 
 @Controller
 public class YoilTeller {
-    @RequestMapping("/yoil")
-    public ModelAndView main(int year, int month, int day) throws IOException {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("yoilError");
+    @RequestMapping("/getYoil")
+    public String main(@ModelAttribute("myDate") MyDate myDate, Model model) throws IOException {
 
-        if (!isVaild(year, month, day)) {
-            return mv;
-        }
+        char yoil = getYoil(myDate);
 
+        return "yoil";
+    }
+
+    @ModelAttribute("yoil")
+    private static char getYoil(MyDate myDate) {
         Calendar cal = Calendar.getInstance();
         cal.clear();
-        cal.set(year, month - 1, day);
+        cal.set(myDate.getYear(), myDate.getMonth() - 1, myDate.getDay());
 
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
         char yoil = "일월화수목금토".charAt(dayOfWeek-1);
-
-        mv.addObject("year", year);
-        mv.addObject("month", month);
-        mv.addObject("day", day);
-        mv.addObject("yoil", yoil);
-        mv.setViewName("yoil");
-
-        return mv;
-    }
-
-    private boolean isVaild(int year, int month, int day) {
-        return true;
+        return yoil;
     }
 }
