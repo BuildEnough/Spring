03d7abb9942d6fa86970fc43073b94a646fadb9d
BuildEnough <rhtmaehcl878@naver.com>
2023-08-09 21:29:16 +0900

    스프링 부트 연습

--- ch2/src/main/java/com/buildenough/ch2/Ch2Application.java
+++ ch2/src/main/java/com/buildenough/ch2/Ch2Application.java
@@ -2,8 +2,10 @@ package com.buildenough.ch2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Ch2Application {
 
 	public static void main(String[] args) {
