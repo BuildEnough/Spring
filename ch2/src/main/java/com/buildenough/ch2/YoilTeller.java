d30e1c3a7c9b4bd2dc94c7ceefb20b5fb83984f3
BuildEnough <rhtmaehcl878@naver.com>
2023-10-02 16:01:55 +0900

    스프링부트 연습

--- ch2/src/main/java/com/buildenough/ch2/YoilTeller.java
+++ ch2/src/main/java/com/buildenough/ch2/YoilTeller.java
@@ -2,21 +2,38 @@ package com.buildenough.ch2;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import java.io.IOException;
+import java.util.Calendar;
 
-import javax.servlet.http.HttpServletRequest;
-
-@RestController
+@Controller
 public class YoilTeller {
-    @RequestMapping("/getYoil")
-    public String main(HttpServletRequest request) {
-        String year = request.getParameter("year");
-        String month = request.getParameter("month");
-        String day = request.getParameter("day");
+    @RequestMapping("/yoil")
+    public ModelAndView main(int year, int month, int day) throws IOException {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("yoilError");
 
-        System.out.println("year = " + year);
-        System.out.println("month = " + month);
-        System.out.println("day = " + day);
-        return "Monday";
+        if(!isValid(year, month, day)) {
+            return mv;
+        }
+
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        cal.set(year, month-1, day);
+
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        char yoil = "일월화수목금토".charAt(dayOfWeek - 1);
+
+        mv.addObject("year", year);
+        mv.addObject("month", month);
+        mv.addObject("day", day);
+        mv.addObject("yoil", yoil);
+        mv.setViewName("yoil");
+
+        return mv;
+    }
+
+    private boolean isValid(int year, int month, int day) {
+        return true;
     }
 }
