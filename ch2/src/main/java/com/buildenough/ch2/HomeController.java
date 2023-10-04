7b19a160b13f66e37e094c02868aabc972a9fb43
BuildEnough <rhtmaehcl878@naver.com>
2023-10-04 22:37:10 +0900

    스프링 부트 연습

--- ch2/src/main/java/com/buildenough/ch2/HomeController.java
+++ ch2/src/main/java/com/buildenough/ch2/HomeController.java
@@ -1,13 +1,25 @@
 package com.buildenough.ch2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String main() {
-
         return "index";
+    }
+
+    @GetMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("lastName", "enough");
+        model.addAttribute("firstName", "build");
+        model.addAttribute("list", Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee"));
+        model.addAttribute("bno", 123);
+        return "test";
     }
 }
