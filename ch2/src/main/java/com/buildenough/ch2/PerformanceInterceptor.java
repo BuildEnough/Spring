7b218c317a68c497728c1d3f85b406561f0449d7
BuildEnough <rhtmaehcl878@naver.com>
2023-10-04 12:14:30 +0900

    스프링 부트 연습

---
+++ ch2/src/main/java/com/buildenough/ch2/PerformanceInterceptor.java
@@ -0,0 +1,35 @@
+package com.buildenough.ch2;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class PerformanceInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+
+        HandlerMethod method = (HandlerMethod)handler;
+        System.out.println("method.getMethod() = " + method.getMethod());
+        System.out.println("method.getBean() = " + method.getBean());
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        long startTime = (long)request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        System.out.print("[" + ((HttpServletRequest)request).getRequestURI() + "]");
+        System.out.println(" time=" + (endTime - startTime));
+
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+}
